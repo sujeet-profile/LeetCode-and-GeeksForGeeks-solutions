38614d9eb57d094fc8a1c03c63016e4949492664
Sujeet Kumar <71321717+glbitm-coder@users.noreply.github.com>
2025-11-21 20:01:24 +0530

    Time: 0 ms (100%), Space: 40.2 MB (66.44%) - LeetHub

--- 3379-score-of-a-string/3379-score-of-a-string.cs
+++ 3379-score-of-a-string/3379-score-of-a-string.cs
@@ -2,7 +2,7 @@
 public class Solution {
     public int ScoreOfString(string str) {
         int ans = 0;
-        for(int i = 1; i < str.Count(); ++i){
+        for(int i = 1; i < str.Length; ++i){
             ans = ans + Math.Abs(str[i] - str[i - 1]);
         }
         return ans;
